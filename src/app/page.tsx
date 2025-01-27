53f5a6dbd1fa51f85f989335b45b222b4ff7ab31
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-01-27 06:06:25 -0800

    update add AddEmployeeForm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { AddEmployeeForm } from "@/components/AddEmployeeForm";
 
 export default function Home() {
   return (
     <div>
       <h1>Employee Managment System</h1>
+      <AddEmployeeForm/>
     </div>
   );
 }
