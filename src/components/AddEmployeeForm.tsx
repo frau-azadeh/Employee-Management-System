6fbed8c84d63d97b8641216e5bbb6884efc7be3d
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-01-27 06:34:26 -0800

    change css

--- src/components/AddEmployeeForm.tsx
+++ src/components/AddEmployeeForm.tsx
@@ -19,37 +19,37 @@ export const AddEmployeeForm: React.FC = () => {
     reset();
  }
     return (
-    <div>
-        <form onSubmit={handleSubmit(onSubmit)} className='p-4 bg-slate-500 rounded-lg shadow'>
-            <h2 className='text-lg font-bold mb-4'>Add Employee</h2>
+    <div className='flex items-center justify-center h-screen '>
+        <form onSubmit={handleSubmit(onSubmit)} className='p-4 bg-slate-400 rounded-lg shadow w-full max-w-md'>
+            <h2 className='text-lg font-bold mb-4 text-slate-900'>Add Employee</h2>
             <div className='mb-4'>
-                <label className='block text-sm font-medium text-gray-700'>Name</label>
+                <label className='block text-md font-medium text-white'>Name: </label>
                 <input
                     {...register("name", {required: true})}
-                    className='mt-1 block w-full border-gray-300 rounded-md shadow-cyan-300'
+                    className='mt-1 block w-full border-gray-300 rounded-md shadow-cyan-300 p-2'
                     placeholder='Enter Name:'
                 />
             </div>
             <div className='mb-4'>
-                <label className='block text-sm font-medium text-gray-700'>Position</label>
+                <label className='block text-md font-medium text-white'>Position: </label>
                 <input
                     {...register("position", {required: true})}
-                    className='mt-1 block w-full border-gray-300 rounded-md shadow-cyan-300'
+                    className='mt-1 block w-full border-gray-300 rounded-md shadow-cyan-300 p-2'
                     placeholder='Enter position'
                 />
             </div>
             <div className='mb-4'>
-                <label className='block text-sm font-medium text-gray-700'>Email: </label>
+                <label className='block text-md font-medium text-white'>Email: </label>
                 <input
                     {...register("position", {required: true})}
-                    className='mt-1 block w-full border-gray-300 rounded-md shadow-cyan-300'
+                    className='mt-1 block w-full border-gray-300 rounded-md shadow-cyan-300 p-2'
                     type="email"
                     placeholder='Enter Email'
                 />
             </div>
             <button
                 type='submit'
-                className='px-4 bg-blue-500 text-white rounded hover:bg-blue-700'
+                className='p-2 bg-slate-700 text-white rounded hover:bg-slate-900'
             >
                 Add Employee
             </button>
