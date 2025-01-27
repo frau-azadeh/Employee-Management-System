ed9e7624dca5c147722a80cfd2fea8a4f53b352e
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-01-27 05:01:23 -0800

    create ContextAPI and addEmployee

---
+++ src/context/EmployeeContext.tsx
@@ -0,0 +1,38 @@
+import { createContext, ReactNode, useContext, useState } from "react";
+interface Employee{
+    id: string;
+    name: string;
+    position: string;
+    email: string;
+}
+
+interface EmployeeContextType{
+    employees: Employee[];
+    addEmployee: (employee: Omit<Employee, "id">) => void;
+}
+
+const EmployeeContext = createContext<EmployeeContextType | undefined>(undefined);
+
+export const EmployeeProvider = ({ children } : {children: ReactNode}) =>{
+    const [employees, setEmployees] = useState <Employee[]>([]);
+
+    const addEmployee = (employee: Omit<Employee, "id">) =>{
+        setEmployees((prev)=>[
+           ...prev,
+          { ...employee, id: Date.now().toLocaleString() } ,
+        ]);
+    };
+    return(
+        <EmployeeContext.Provider value= {{employees, addEmployee}} >
+            {children}
+        </EmployeeContext.Provider>
+    );
+};
+
+export const useEmployeeContext = () =>{
+    const context = useContext(EmployeeContext);
+        if(!context){
+            throw new Error("useEmployeeContext must be use within an EmployeeProvider");
+    }
+    return context;
+}
