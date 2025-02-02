fd88a5ecdab62b89c09d2c7b20078b4bc30bae9e
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-02 00:04:22 -0800

    update css table

--- src/components/EmployeeTable.tsx
+++ src/components/EmployeeTable.tsx
@@ -17,17 +17,17 @@ const EmployeeTable: React.FC<EmployeeTableProps> = ({ employees }) => {
   return (
     <div className="overflow-x-auto">
       <table className="min-w-full bg-white border  border-gray-300 shadow-lg rounded-lg">
-        <thead className="bg-gray-700 text-white">
+        <thead className="bg-gray-700 text-white text-center">
           <tr>
-            <th className="px-4 py-3 text-left">Name</th>
-            <th className="px-4 py-3 text-left">Position</th>
-            <th className="px-4 py-3 text-left">Email</th>
+            <th className="px-4 py-3 ">Name</th>
+            <th className="px-4 py-3 ">Position</th>
+            <th className="px-4 py-3 ">Email</th>
           </tr>
         </thead>
         <tbody>
           {employees.length > 0 ? (
             employees.map((employee) => (
-              <tr key={employee.id} className="hover:bg-gray-100">
+              <tr key={employee.id} className="hover:bg-gray-700 hover:text-white text-center">
                 <td className="border px-4 py-2">{employee.name}</td>
                 <td className="border px-4 py-2">{employee.position}</td>
                 <td className="border px-4 py-2">{employee.email}</td>
