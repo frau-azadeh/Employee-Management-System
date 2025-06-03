1a27a8e48737b65ab41d5698460860af9b83dc3b
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-06-03 14:43:42 +0330

    add GoBack button component

---
+++ src/components/GoBack.tsx
@@ -0,0 +1,16 @@
+'use client'
+import { useRouter } from 'next/navigation'
+import React from 'react'
+
+const GoBack = () => {
+    const router = useRouter();
+  return (
+    <button
+        onClick={()=>router.back()}
+    >
+        Go Back
+    </button>
+)
+}
+
+export default GoBack
