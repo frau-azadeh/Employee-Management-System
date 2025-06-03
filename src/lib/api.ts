8d0bd1774cdeaf58f64cdc09267fcaaa7208de46
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-06-03 17:55:21 +0330

    update

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,6 @@
 import { PostPage } from "@/type/type";
 import axios from "axios";
+import { AxiosError } from "axios";
 
 const jsonAPI = axios.create({
     baseURL: "https://jsonplaceholder.typicode.com",
@@ -10,3 +11,24 @@ export async function getPost(): Promise<PostPage[]> {
     const res = await jsonAPI.get<PostPage[]>("/posts");
     return res.data
 }
+
+export async function getPostById(id: string): Promise<PostPage | null> {
+    const postId = Number(id);
+    if (isNaN(postId)) return null;
+  
+    try {
+      const res = await jsonAPI.get<PostPage>(`/posts/${postId}`);
+      return res.data;
+    } catch (error) {
+      const err = error as AxiosError;
+  
+      if (err.response?.status === 404) {
+        console.warn(`This post with id=${postId} not found`);
+        return null;
+      }
+  
+      console.error("error with get post", err.message);
+      return null;
+    }
+  }
+  
