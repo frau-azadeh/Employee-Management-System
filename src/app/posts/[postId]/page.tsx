7f70465fca7ce52f71aa9ee078c5fe2eda02c9c8
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-06-05 14:17:17 +0330

    update css

--- src/app/posts/[postId]/page.tsx
+++ src/app/posts/[postId]/page.tsx
@@ -1,28 +1,26 @@
 import GoBack from "@/components/GoBack";
-import { getPostById } from "@/lib/api";
+import { getPostById } from "@/lib/api"
 import { notFound } from "next/navigation";
 
-interface PostPageProps {
-  params: {
-    postId: string;
-  };
+interface PostPageProps{
+  params:{
+    postId: string
+  }
 }
 
-export default async function SinglePostPage({ params }: PostPageProps) {
+export default async function SinglePostPage({params}: PostPageProps) {
   const post = await getPostById(params.postId);
 
-  if (!post) {
+  if (!post){
     notFound();
   }
-
-  return (
-    <div className=" flex flex-col mx-auto  justify-center h-screen">
-      <div className="bg-white p-5 rounded-xl shadow hover:scale-105">
-        <h1 className="text-blue-500 mt-5 mb-5">{post.title}</h1>
-        <p className="text-gray-700 text-base">{post.body}</p>
-      </div>
-      <GoBack />
+  return(
+    <div className="flex flex-col mx-auto justify-center h-screen">
+      <div className="rounded-xl bg-white shadow hover:scale-105 mb-5 p-5">
+      <h1 className="text-blue-400 mt-5 mb-5">{post.title}</h1>
+      <p className="text-gray-800 text-base">{post.body}</p>
     </div>
-  );
+    <GoBack/>
+    </div>
+  )
 }
-
