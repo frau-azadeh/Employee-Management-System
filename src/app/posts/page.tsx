7999c063c774131a3c37049e9be2fa65b21f0514
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-06-05 14:17:09 +0330

    update css

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,26 +1,24 @@
 import { getPost } from "@/lib/api";
-import { ChevronsRightIcon } from "lucide-react";
+import { ChevronRightIcon } from "lucide-react";
 import Link from "next/link";
+
 export default async function PostsPage() {
   const posts = await getPost();
   return (
     <div className="grid grid-cols-3 gap-5 mt-10">
       {posts.map((post) => (
         <div
+          className="bg-white p-5 rounded-lg shadow hover:scale-105 transition duration-300"
           key={post.id}
-          className="rounded-lg bg-white p-4 shadow hover:scale-105"
         >
-          <span className="text-blue-500 line-clamp-1 font-bold mb-2">
-            {post.title}
-          </span>
-          <p className="text-gray-800 text-base line-clamp-3 leading-6">
-            {post.body}
-          </p>
+          <h2 className="text-blue-400 line-clamp-1 mb-3 ">{post.title}</h2>
+          <p className="text-gray-500 line-clamp-3 mb-3">{post.body}</p>
           <Link
             href={`/posts/${post.id}`}
-            className="text-xs text-blue-700 hover:text-blue-900 transition duration-300 flex items-end gap-6 mt-3"
+            className="flex text-blue-500 hover:text-blue-700 text-xs gap-5 items-center font-bold"
           >
-            Read More <ChevronsRightIcon className="w-4 h-4" />
+            Read More
+            <ChevronRightIcon className="w-4 h-4" />
           </Link>
         </div>
       ))}
