d1d49d0315380d8b08f6657403a340cef4c8dc8f
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-06-05 14:17:46 +0330

    update prettier

--- src/app/posts/[postId]/page.tsx
+++ src/app/posts/[postId]/page.tsx
@@ -1,26 +1,26 @@
 import GoBack from "@/components/GoBack";
-import { getPostById } from "@/lib/api"
+import { getPostById } from "@/lib/api";
 import { notFound } from "next/navigation";
 
-interface PostPageProps{
-  params:{
-    postId: string
-  }
+interface PostPageProps {
+  params: {
+    postId: string;
+  };
 }
 
-export default async function SinglePostPage({params}: PostPageProps) {
+export default async function SinglePostPage({ params }: PostPageProps) {
   const post = await getPostById(params.postId);
 
-  if (!post){
+  if (!post) {
     notFound();
   }
-  return(
+  return (
     <div className="flex flex-col mx-auto justify-center h-screen">
       <div className="rounded-xl bg-white shadow hover:scale-105 mb-5 p-5">
-      <h1 className="text-blue-400 mt-5 mb-5">{post.title}</h1>
-      <p className="text-gray-800 text-base">{post.body}</p>
+        <h1 className="text-blue-400 mt-5 mb-5">{post.title}</h1>
+        <p className="text-gray-800 text-base">{post.body}</p>
+      </div>
+      <GoBack />
     </div>
-    <GoBack/>
-    </div>
-  )
+  );
 }
